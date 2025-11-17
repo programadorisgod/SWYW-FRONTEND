9c9f9516a178653b214d62f60c88206054be629a
programadorisgod <catapiascastro@gmail.com>
2025-11-17 13:53:01 -0500

    fix: load vault secrets

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
-envsubst '${VITE_API_AUTH},${VITE_API_NOTES}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-nginx -g "daemon off;"
+set -e
+
+# 1. Cargar variables inyectadas por Vault
+if [ -f /vault/secrets/frontend.env ]; then
+  echo "[entrypoint] Loading Vault secrets..."
+  . /vault/secrets/frontend.env
+fi
+
+# 2. Renderizar config con envsubst
+echo "[entrypoint] Rendering NGINX config..."
+envsubst '${VITE_API_AUTH} ${VITE_API_NOTES}' \
+  < /etc/nginx/conf.d/default.conf.template \
+  > /etc/nginx/conf.d/default.conf
+
+# 3. Iniciar NGINX
+echo "[entrypoint] Starting NGINX..."
+exec nginx -g 'daemon off;'
